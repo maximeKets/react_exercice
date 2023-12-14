7e8661ac26e29a88c9fd0419a9eafdf6d7938ebb
maximeKets <maxime.kets@le-campus-numerique.fr>
2023-12-14 12:25:55 -0500

    feat : add tailwindscss feat : create template for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
 export function App() {
-    return <div>Hello World</div>
+    return <div className="flex flex-col h-screen justify-evenly">
+        <div className="grid grid-cols-4">
+            <div>Logo</div>
+            <div className="col-span-2 w-full broder border-2">Recherche</div>
+            <div></div>
+        </div>
+        <div>
+            info sur le film
+        </div>
+        <div className="w-full border border-2"> slider</div>
+    </div>
 }
